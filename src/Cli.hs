817b7b29d3e3237f206e94baebf6ac342f1d0cd8
sergeylyashko <slyashko@mail.ru>
2025-09-27 23:14:44 +0300

    add todo

--- src/Cli.hs
+++ src/Cli.hs
@@ -1,4 +1,9 @@
 module Cli (cliParser) where
 
+{-
+	todo Продумать вариант передавать некий предикат группировки файлов
+	по разным директориям
+
+-}
 cliParser :: [String] -> IO ()
 cliParser = mapM_ putStrLn
--- src/Files/FilesIO.hs
+++ src/Files/FilesIO.hs
@@ -1,8 +1,44 @@
 module Files.FilesIO (
-
+    printFileExt,
+    flatDirectories,
 ) where
 
+import Control.Monad (guard, when)
+import System.Directory (doesDirectoryExist, doesFileExist, getDirectoryContents)
+import System.FilePath (takeExtensions, takeFileName)
 import System.IO ()
+import System.Posix (fileMode, fileSize, getFileStatus, isDirectory, modificationTime)
 
-flatFolders :: FilePath -> IO ()
-flatFolders path = undefined
+printFileExt :: FilePath -> IO ()
+printFileExt path = do
+    let ext = takeExtensions path
+    let name = takeFileName path
+    putStrLn $ name ++ " ext: " ++ ext
+
+{-
+    Алгоритм рекурсивного уплощения директорий:
+    0. Проверяем все файлы уровня
+    1. Проверяем является ли файл директорией
+    2. Если да - вход в диреторию, выполнение с п.1
+    3. Иначе - переносим все файлы выше на уровень если этот уровень не корневой
+    4. Если уровень корневой ничего не делаем
+-}
+type DirPath = String
+
+flatDirectories :: DirPath -> IO ()
+flatDirectories path =
+    do
+        contents <- getDirectoryContents path
+        directoryContetns $ filter (\c -> c /= "." && c /= "..") contents
+
+-- mapM_ putStrLn contents
+
+directoryContetns :: [FilePath] -> IO ()
+directoryContetns [] = return ()
+directoryContetns (path : paths) = undefined
+
+isFile :: FilePath -> Bool
+isFile path = undefined
+
+isDir :: FilePath -> Bool
+isDir path = undefined
