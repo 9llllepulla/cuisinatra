cf5dc69b9d1536c23c07fddaa12a0f4ae04c72f8
sergeylyashko <slyashko@mail.ru>
2025-09-30 23:16:22 +0300

    change flatDirectories on io strings

--- src/Files/FlatDirectory.hs
+++ src/Files/FlatDirectory.hs
@@ -1,10 +1,16 @@
 module Files.FlatDirectory (
     flatDirectories,
+    printFiles,
 ) where
 
 import Data.List.Utils
 import System.Directory (doesDirectoryExist, doesFileExist, getDirectoryContents)
 import System.IO ()
+
+printFiles :: FilePath -> IO ()
+printFiles path = do
+    dirs <- flatDirectories path
+    mapM_ putStrLn dirs
 
 data Path = File FilePath | Dir FilePath
 
@@ -16,8 +22,7 @@ data Path = File FilePath | Dir FilePath
     3. Иначе - переносим все файлы выше на уровень если этот уровень не корневой
     4. Если уровень родительский - ничего не делаем
 -}
-
-flatDirectories :: FilePath -> IO ()
+flatDirectories :: FilePath -> IO [FilePath]
 flatDirectories path =
     do
         dirContents <- getDirectoryContents path
@@ -26,12 +31,13 @@ flatDirectories path =
     fromContents = filter isNotParentDir
     isNotParentDir = not . flip endswith ".."
 
-directoryContetns :: [FilePath] -> IO ()
-directoryContetns [] = return ()
+directoryContetns :: [FilePath] -> IO [FilePath]
+directoryContetns [] = return []
 directoryContetns (path : paths) = do
     mPath <- toPath path
-    printFile mPath
-    directoryContetns paths
+    fPaths <- printFile mPath
+    restPaths <- directoryContetns paths
+    return $ fPaths ++ restPaths
 
 toPath :: FilePath -> IO (Maybe Path)
 toPath path = do
@@ -44,7 +50,7 @@ toPath path = do
                 then return $ Just $ File path
                 else return Nothing
 
-printFile :: Maybe Path -> IO ()
-printFile Nothing = return ()
+printFile :: Maybe Path -> IO [FilePath]
+printFile Nothing = return []
 printFile (Just (Dir path)) = flatDirectories path
-printFile (Just (File path)) = putStrLn path
+printFile (Just (File path)) = return [path]
