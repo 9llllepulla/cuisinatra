dc30c1e502a42a37710032342c0d6fb05ad5a2b9
sergeylyashko <slyashko@mail.ru>
2025-10-06 23:22:14 +0300

    add func for move files to dir

--- src/Files/FlatDirectory.hs
+++ src/Files/FlatDirectory.hs
@@ -1,24 +1,29 @@
-{- |
-    Алгоритм уплощения директорий:
+module Files.FlatDirectory (
+    printFiles,
+    moveFiles,
+) where
+
+import Data.List.Utils (endswith)
+import System.Directory (doesDirectoryExist, doesFileExist, getDirectoryContents, renameFile)
+import System.FilePath (takeFileName)
+import System.IO ()
+
+printFiles :: FilePath -> IO ()
+printFiles path = extractFilesPaths path >>= mapM_ putStrLn
+
+moveFiles :: [FilePath] -> FilePath -> IO ()
+moveFiles paths dir = moveFilesToDirectory paths (DirPath dir)
+
+newtype DirPath = DirPath FilePath
+
+{-
+    Алгоритм извлечения файлов из директорий:
     0. Проверяем все файлы уровня
     1. Проверяем является ли файл директорией
     2. Если да - вход в диреторию, выполнение п.0
     3. Иначе - переносим все файлы выше на уровень если этот уровень не корневой
     4. Если уровень родительский - ничего не делаем
 -}
-module Files.FlatDirectory (
-    printFiles,
-) where
-
-import Data.List.Utils (endswith)
-import System.Directory (doesDirectoryExist, doesFileExist, getDirectoryContents)
-import System.IO ()
-
-printFiles :: FilePath -> IO ()
-printFiles path = extractFilesPaths path >>= mapM_ putStrLn
-
-newtype DirPath = DirPath FilePath
-
 extractFilesPaths :: FilePath -> IO [FilePath]
 extractFilesPaths path = do
     isDir <- doesDirectoryExist path
@@ -45,3 +50,17 @@ getAllFilesPaths (DirPath path) = do
     mconcat $ map extractFilesPaths fullPaths
   where
     getNotParentDirContentsFrom = filter $ not . flip endswith ".."
+
+{-
+    todo
+    2. Фильтрация по 1 условию получаемых файлов
+    3. Заменить проверку родительской директории ".." на проверку пути ?
+-}
+
+-- Перемещение файла
+-- todo нужно возвращать список файлов
+moveFilesToDirectory :: [FilePath] -> DirPath -> IO ()
+moveFilesToDirectory paths (DirPath dirPath) = do
+    mapM_ (\oldName -> renameFile oldName (newName oldName)) paths
+  where
+    newName path = dirPath <> takeFileName path
