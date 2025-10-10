c79908ac7c4f7b6c16fc7f65a2f2749d979438cc
sergeylyashko <slyashko@mail.ru>
2025-10-10 21:53:05 +0300

    update code of FlatDirectory

--- src/Files/FlatDirectory.hs
+++ src/Files/FlatDirectory.hs
@@ -4,9 +4,7 @@ module Files.FlatDirectory (
     testExtractTypeFiles,
     testMoveTypeFiles,
     Directory,
-    extractFiles,
     extractTypeFiles,
-    moveFilesToDirectory,
     moveTypeFilesToDirectory,
 ) where
 
@@ -31,13 +29,40 @@ testMoveTypeFiles fType sourceDir goalDir =
     moveTypeFilesToDirectory fType (Directory sourceDir) (Directory goalDir) >>= mapM_ putStrLn
 
 ------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
 
 type FileType = String
 newtype Directory = Directory FilePath
 
 {- |
-    Извлечение файлов из дерева директорий
+    Извлечение файлов заданного типа из дерева директорий
 -}
+extractTypeFiles :: FileType -> Directory -> IO [FilePath]
+extractTypeFiles fType (Directory path) = do
+    files <- extractFiles path
+    return $ filesFilter fType files
+
+{- |
+     Перемещение файлов заданного типа из указанной директории в новую
+-}
+moveTypeFilesToDirectory :: FileType -> Directory -> Directory -> IO [FilePath]
+moveTypeFilesToDirectory fType sourceDir goalDir = do
+    files <- getAllFiles sourceDir
+    let goalFiles = filesFilter fType files
+    moveFilesToDirectory goalFiles goalDir
+
+------------------------------------------------------------------------------------------------
+
+-- | Перемещение файлов в новую директорию
+moveFilesToDirectory :: [FilePath] -> Directory -> IO [FilePath]
+moveFilesToDirectory files (Directory dir) = do
+    mapM_ (\(old, new) -> renameFile old new) oldNewPaths
+    return $ map snd oldNewPaths
+  where
+    oldNewPaths = map (\oldName -> (oldName, toNewName oldName)) files
+    toNewName file = dir <> takeFileName file
+
+-- | Извлечение файлов из дерева директорий
 extractFiles :: FilePath -> IO [FilePath]
 extractFiles path = do
     isDir <- doesDirectoryExist path
@@ -54,37 +79,7 @@ extractFiles path = do
             then path
             else path <> "/"
 
-{- |
-    Извлечение файлов заданного типа из дерева директорий
--}
-extractTypeFiles :: FileType -> Directory -> IO [FilePath]
-extractTypeFiles fType (Directory path) = do
-    files <- extractFiles path
-    return $ filesFilter fType files
-
-{-
-     Перемещение файлов в новую директорию
--}
-moveFilesToDirectory :: [FilePath] -> Directory -> IO [FilePath]
-moveFilesToDirectory files (Directory dir) = do
-    mapM_ (\(old, new) -> renameFile old new) oldNewPaths
-    return $ map snd oldNewPaths
-  where
-    oldNewPaths = map (\oldName -> (oldName, toNewName oldName)) files
-    toNewName file = dir <> takeFileName file
-
-{-
-     Перемещение файлов заданного типа из указанной директории в новую
--}
-moveTypeFilesToDirectory :: FileType -> Directory -> Directory -> IO [FilePath]
-moveTypeFilesToDirectory fType sourceDir goalDir = do
-    files <- getAllFiles sourceDir
-    let goalFiles = filesFilter fType files
-    moveFilesToDirectory goalFiles goalDir
-
-{- |
-    Получение всех файлов дерева директории (со всеми поддиректориями)
--}
+-- | Получение всех файлов дерева директории (со всеми поддиректориями)
 getAllFiles :: Directory -> IO [FilePath]
 getAllFiles (Directory path) = do
     contents <- getDirectoryContents path
@@ -93,9 +88,6 @@ getAllFiles (Directory path) = do
   where
     withoutParentDirContents = filter $ not . flip endswith ".."
 
-{- |
-    Фильтрация по типу файла
-    -- todo заменить на проверку по типу файла
--}
+-- | Фильтрация по типу файла
 filesFilter :: FileType -> [FilePath] -> [FilePath]
 filesFilter fileType = filter $ \path -> fileType `endswith` path
